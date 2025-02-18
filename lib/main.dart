2ac68c00e36d23b7e58198a0592cc799467776ee
here-comes-ivy <ivyhsu0919@gmail.com>
2025-02-18 12:02:09 -0500

    updating font to NotoSans & cache back to sharedpreferences

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,7 @@ void main() async {
         debugPrint('LIFF init success.');
       },
       errorCallback: (error) {
-        debugPrint(
-            'LIFF init error: $error');
+        debugPrint('LIFF init error: $error');
       },
     );
     await FlutterLineLiff.instance.ready;
@@ -48,38 +47,37 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-          print(GlobalLiffData.landmarkDetails);
+    print(GlobalLiffData.landmarkDetails);
     return MaterialApp(
-      title: "「一府 x iF」遊城活動打卡",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: const Color.fromRGBO(218, 188, 76, 1),
+        title: "「一府 x iF」遊城活動打卡",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'NotoSans',
+          primaryColor: const Color.fromRGBO(218, 188, 76, 1),
 
-        //useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white, // 修正顏色值格式
-        //scaffoldBackgroundColor: const Color.fromRGBO(242, 239, 233, 1),
-        textTheme: Theme.of(context).textTheme.apply(
-              bodyColor: const Color(0xFF507166), // 修正顏色值格式
-              displayColor: const Color(0xFF507166), // 修正顏色值格式
+          //useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white, // 修正顏色值格式
+          //scaffoldBackgroundColor: const Color.fromRGBO(242, 239, 233, 1),
+          textTheme: Theme.of(context).textTheme.apply(
+                bodyColor: const Color(0xFF507166), // 修正顏色值格式
+                displayColor: const Color(0xFF507166), // 修正顏色值格式
+              ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            shadowColor: Colors.black.withValues(alpha: 0.2),
+            elevation: 10,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              color: const Color(0xFF507166),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          shadowColor: Colors.black.withValues(alpha: 0.2),
-          elevation: 10,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            color: const Color(0xFF507166),
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
           ),
+          splashColor: const Color(0xFF507166),
+          // colorScheme: ColorScheme.fromSeed(
+          //   seedColor: Colors.white,
+          // ),
         ),
-        splashColor: const Color(0xFF507166),
-        // colorScheme: ColorScheme.fromSeed(
-        //   seedColor: Colors.white,
-        // ),
-      ),
-
-      home: LandingPage() 
-    );
+        home: LandingPage());
   }
 }
